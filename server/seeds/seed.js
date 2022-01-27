299dff54d706464e17f3bf846b52683c50c81b2f
Matt Grogan <mattgrogan.dev@gmail.com>
2022-01-27 14:29:55 -0700

    added schemas & wording for seed file

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,15 +1,29 @@
 const db = require("../config/connection");
-const { Profile } = require("../models");
-const profileSeeds = require("./profileSeeds.json");
+const { Tech } = require("../models");
+
+const techData = require("./techData.json");
 
 db.once("open", async () => {
-  try {
-    await Profile.deleteMany({});
-    await Profile.create(profileSeeds);
+  await Tech.deleteMany({});
 
-    console.log("all done!");
-    process.exit(0);
-  } catch (err) {
-    throw err;
-  }
+  const technologies = await Tech.insertMany(techData);
+
+  console.log("Technologies seeded!");
+  process.exit(0);
 });
+
+// const db = require("../config/connection");
+// const { Profile } = require("../models");
+// const profileSeeds = require("./profileSeeds.json");
+
+// db.once("open", async () => {
+//   try {
+//     await Profile.deleteMany({});
+//     await Profile.create(profileSeeds);
+
+//     console.log("all done!");
+//     process.exit(0);
+//   } catch (err) {
+//     throw err;
+//   }
+// });
