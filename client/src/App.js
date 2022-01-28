1c77312a77ee283c4639f302fe3eaadcbb755a02
Kerry Eames <Kerryeames@live.com>
2022-01-28 12:57:18 -0700

    created navbar/header, moved tailwindCSS files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
+import "./styles/output.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Navbar />
-      <Card />
-      <Card />
-      <Card />
     </div>
   );
 }
