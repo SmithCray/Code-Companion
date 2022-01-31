32f87cc21b68b4a93f6d280a35e03198421f69c3
Collin Kerr <collin.kerr@gmail.com>
2022-01-31 10:24:12 -0700

    file structure for pages

---
+++ client/src/pages/search.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "../styles/output.css";
+import Header from "../components/Header"
+
+function signUp (){
+    return ( <> 
+    <Header />
+     </>
+
+
+
+
+
+    )
+
+
+}
