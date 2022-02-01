c5c00773982bf191433cd47051c2a3b0f6843ceb
Collin Kerr <collin.kerr@gmail.com>
2022-02-01 13:54:17 -0700

    fixed errors

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -4,21 +4,19 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "../styles/output.css";
 import Header from "../components/Header";
 
+const buttonCSS =  "absolute bottom-0 left-1/3 text-2xl px-3 mx-2 my-12 py-2 bg-gradient-to-r from-sky-400 to-blue-700 rounded-lg hover:shadow hover:bg-gradient-to-r hover:from-sky-500 hover:to-blue-800";
+function Landing() {
   return (
     <>
       <Header />
-      <div className="bg-[url('../styles/img/landing.png')] pt-16">
+      <div className="bg-[url('../styles/img/landing.png')] pt-16 w-screen">
         <div className="flex justify-center">
           <div className="relative text-center mx-2 my-2 h-96 w-1/4 border rounded-md border-black border-2">
             <h1 className="text-4xl mx-2 my-2">Ready To Get Started?</h1>
             <Link
               to="/Signup"
               className={buttonCSS}
-              onClick={() =>
-                loginWithRedirect({
-                  screen_hint: "signup",
-                })
-              }
+              
             >
               SignUp Here
             </Link>
